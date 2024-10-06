804f1fa19ddca16c05d44e2972535bf005f080f2
Cosmin Trusca <mariancosmintrusca@gmail.com>
2024-10-06 09:57:17 +0000

    Build the post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -89,6 +89,13 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+    "https://8000-cosmin2002tr-djangoblog-hml3mbbt93t.ws.codeinstitute-ide.net"
+    "https://django-----blog-f8f0fd338a5f.herokuapp.com/"
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
